6a935b768a0d99a8d2b043eebce4e26ee404eaf1
shuisheng <2274751790@qq.com>
2023-12-26 20:15:58 +0800

    feat: interface

--- src/interface.ts
+++ src/interface.ts
@@ -1,3 +1,299 @@
+// ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ 直播间类型 ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+
+/** 是否使用cdn */
+export enum LiveRoomUseCDNEnum {
+  /** 使用cdn */
+  yes,
+  /** 不使用cdn */
+  no,
+}
+
+/** 直播间类型 */
+export enum LiveRoomTypeEnum {
+  /** 系统直播 */
+  system,
+  /** 主播使用webrtc直播 */
+  user_wertc,
+  /** 主播使用srs直播 */
+  user_srs,
+  /** 主播使用obs/ffmpeg直播 */
+  user_obs,
+  /** 主播使用msr直播 */
+  user_msr,
+}
+
+/** 拉流是否需要鉴权 */
+export enum LiveRoomPullIsShouldAuthEnum {
+  /** 需要鉴权 */
+  yes,
+  /** 不需要鉴权 */
+  no,
+}
+
+/** 直播间状态 */
+export enum LiveRoomStatusEnum {
+  /** 正常 */
+  normal,
+  /** 禁用 */
+  disable,
+}
+
+/** 直播间是否显示 */
+export enum LiveRoomIsShowEnum {
+  /** 显示 */
+  yes,
+  /** 不显示 */
+  no,
+}
+
+/** 直播间是否开启聊天 */
+export enum LiveRoomOpenChatEnum {
+  yes,
+  no,
+}
+
+/** 游客能否发言 */
+export enum LiveRoomTouristSendMsgEnum {
+  yes,
+  no,
+}
+
+/** 是否显示弹窗广告 */
+export enum LiveRoomIsShowAdEnum {
+  yes,
+  no,
+}
+
+/** 关闭房间 */
+export enum LiveRoomIsCloseEnum {
+  yes,
+  no,
+}
+
+/** 提醒游客登录 */
+export enum LiveRoomTipTouristLoginEnum {
+  /** 关闭，即不提醒游客登录 */
+  close,
+  /** 开启提醒游客登录，游客可关闭 */
+  open,
+  /** 强制游客登录，即游客不可关闭 */
+  force,
+}
+
+export interface ILiveRoom {
+  id?: number;
+  /** 直播间名称 */
+  name?: string;
+  /** 直播间简介 */
+  desc?: string;
+  /** 直播间备注 */
+  remark?: string;
+  /** 是否使用cdn */
+  cdn?: LiveRoomUseCDNEnum;
+  /** 拉流是否需要鉴权 */
+  pull_is_should_auth?: LiveRoomPullIsShouldAuthEnum;
+  /** 权重 */
+  weight?: number;
+  /** 推流秘钥 */
+  key?: string;
+  /** 直播间类型 */
+  type?: LiveRoomTypeEnum;
+  /** 开播预览图 */
+  cover_img?: string;
+  /** 直播间背景图 */
+  bg_img?: string;
+  /** 直播间状态 */
+  status?: LiveRoomStatusEnum;
+  /** 直播间是否显示 */
+  is_show?: LiveRoomIsShowEnum;
+  /** 直播间是否开启聊天 */
+  open_chat?: LiveRoomOpenChatEnum;
+  /** 提醒游客登录 */
+  tip_tourist_login?: LiveRoomTipTouristLoginEnum;
+  /** 提醒游客登录间隔 */
+  tip_tourist_login_delay?: number;
+  /** 游客能否发言 */
+  tourist_send_msg?: LiveRoomTouristSendMsgEnum;
+  /** 聊天关键词过滤 */
+  keyword_filter_msg?: string;
+  /** 用户名关键词过滤 */
+  keyword_filter_username?: string;
+  /** 历史消息条数 */
+  history_msg_total?: number;
+  /** 新注册用户不能发言时间(秒) */
+  newuser_send_msg_delay?: number;
+  /** 房间密码 */
+  room_pwd?: string;
+  /** 显示弹窗广告 */
+  is_show_ad?: LiveRoomIsShowAdEnum;
+  /** 弹窗广告图片 */
+  ad_img_url?: string;
+  /** 关闭房间 */
+  is_close?: LiveRoomIsCloseEnum;
+  /** 关闭房间描述 */
+  is_close_desc?: string;
+  rtmp_url?: string;
+  flv_url?: string;
+  hls_url?: string;
+
+  /** 用户信息 */
+  user?: IUser;
+  /** 用户信息 */
+  users?: IUser[];
+  /** 分区信息 */
+  area?: IArea;
+  /** 分区信息 */
+  areas?: IArea[];
+  /** 直播信息 */
+  live?: ILive;
+  user_live_room?: IUserLiveRoom & { user: IUser };
+
+  created_at?: string;
+  updated_at?: string;
+  deleted_at?: string;
+}
+
+// ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ 直播间类型 ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+
+// ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ 用户类型 ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+
+export interface IQqUser {
+  id?: number;
+  client_id?: number;
+  openid?: string;
+  unionid?: string;
+  nickname?: string;
+  figureurl?: string;
+  figureurl_1?: string;
+  figureurl_2?: string;
+  figureurl_qq_1?: string;
+  figureurl_qq_2?: string;
+  constellation?: string;
+  gender?: string;
+  city?: string;
+  province?: string;
+  year?: string;
+  ret?: number;
+  created_at?: string;
+  updated_at?: string;
+  deleted_at?: string;
+}
+
+export interface IWechatUser {
+  id?: number;
+  appid?: string;
+  openid?: string;
+  nickname?: string;
+  sex?: number;
+  province?: string;
+  city?: string;
+  country?: string;
+  headimgurl?: string;
+  privilege?: string;
+  unionid?: string;
+  created_at?: string;
+  updated_at?: string;
+  deleted_at?: string;
+}
+
+export interface IEmailUser {
+  id?: number;
+  email?: string;
+  created_at?: string;
+  updated_at?: string;
+  deleted_at?: any;
+}
+
+export interface IGithubUser {
+  id?: number;
+  client_id?: string;
+  login?: string;
+  github_id?: number;
+  node_id?: string;
+  avatar_url?: string;
+  gravatar_id?: string;
+  url?: string;
+  html_url?: string;
+  type?: string;
+  site_admin?: string;
+  name?: string;
+  company?: string;
+  blog?: string;
+  location?: string;
+  email?: any;
+  hireable?: any;
+  bio?: string;
+  twitter_username?: any;
+  public_repos?: number;
+  public_gists?: number;
+  followers?: number;
+  following?: number;
+  github_created_at?: string;
+  github_updated_at?: string;
+  private_gists?: number;
+  total_private_repos?: number;
+  owned_private_repos?: number;
+  disk_usage?: number;
+  collaborators?: number;
+  two_factor_authentication?: string;
+  created_at?: string;
+  updated_at?: string;
+  deleted_at?: any;
+}
+
+export interface IThirdUser {
+  id?: number;
+  user_id?: number;
+  third_user_id?: number;
+  third_platform?: number;
+  created_at?: string;
+  updated_at?: string;
+  deleted_at?: string;
+}
+
+/** 用户状态 */
+export enum UserStatusEnum {
+  /** 正常 */
+  normal,
+  /** 禁用 */
+  disable,
+}
+
+export enum UserIsTouristEnum {
+  yes,
+  no,
+}
+
+export interface IUser {
+  id?: number;
+  username?: string;
+  password?: string;
+  status?: UserStatusEnum;
+  avatar?: string;
+  desc?: string;
+  token?: string;
+  is_tourist?: UserIsTouristEnum;
+
+  qq_users?: IQqUser[];
+  wechat_users?: IWechatUser[];
+  github_users?: IGithubUser[];
+  email_users?: IEmailUser[];
+
+  roles?: any[];
+  auths?: any[];
+
+  user_roles?: number[];
+  wallet?: IWallet;
+
+  live_room?: ILiveRoom;
+  live_rooms?: ILiveRoom[];
+
+  created_at?: string;
+  updated_at?: string;
+  deleted_at?: string;
+}
+// ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ 用户类型 ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+
 export interface ISearchBase {
   keyWord?: string;
   nowPage?: string;
@@ -90,15 +386,6 @@ export interface IApiV1Clients {
   }[];
 }
 
-export interface IThirdUser {
-  id?: number;
-  user_id?: number;
-  third_user_id?: number;
-  third_platform?: number;
-  created_at?: string;
-  updated_at?: string;
-  deleted_at?: string;
-}
 export interface IStar {
   id?: number;
   article_id?: number;
@@ -282,50 +569,6 @@ export interface IMonit {
   info?: string;
 }
 
-export interface IEmailUser {
-  id?: number;
-  email?: string;
-  created_at?: string;
-  updated_at?: string;
-  deleted_at?: any;
-}
-export interface IGithubUser {
-  id?: number;
-  client_id?: string;
-  login?: string;
-  github_id?: number;
-  node_id?: string;
-  avatar_url?: string;
-  gravatar_id?: string;
-  url?: string;
-  html_url?: string;
-  type?: string;
-  site_admin?: string;
-  name?: string;
-  company?: string;
-  blog?: string;
-  location?: string;
-  email?: any;
-  hireable?: any;
-  bio?: string;
-  twitter_username?: any;
-  public_repos?: number;
-  public_gists?: number;
-  followers?: number;
-  following?: number;
-  github_created_at?: string;
-  github_updated_at?: string;
-  private_gists?: number;
-  total_private_repos?: number;
-  owned_private_repos?: number;
-  disk_usage?: number;
-  collaborators?: number;
-  two_factor_authentication?: string;
-  created_at?: string;
-  updated_at?: string;
-  deleted_at?: any;
-}
-
 export interface IWallet {
   id?: number;
   user_id?: number;
@@ -414,41 +657,6 @@ export interface IOrder {
   trade_status?: PayStatusEnum;
   /** 本次交易打款给卖家的时间 */
   send_pay_date?: string;
-  created_at?: string;
-  updated_at?: string;
-  deleted_at?: string;
-}
-
-/** 用户状态 */
-export enum UserStatusEnum {
-  /** 正常 */
-  normal,
-  /** 禁用 */
-  disable,
-}
-
-export interface IUser {
-  id?: number;
-  username?: string;
-  password?: string;
-  email?: string;
-  status?: UserStatusEnum;
-  avatar?: string;
-  desc?: string;
-  token?: string;
-
-  wallet?: IWallet;
-  live_room?: ILiveRoom;
-  live_rooms?: ILiveRoom[];
-
-  roles?: IRole[];
-  auths?: IAuth[];
-  user_roles?: number[];
-
-  qq_users?: IQqUser[];
-  github_users?: IGithubUser[];
-  email_users?: IEmailUser[];
-
   created_at?: string;
   updated_at?: string;
   deleted_at?: string;
@@ -573,114 +781,6 @@ export interface IUserLiveRoom {
   created_at?: string;
   updated_at?: string;
   deleted_at?: string;
-}
-
-/** 直播间类型 */
-export enum LiveRoomTypeEnum {
-  system, // 系统直播
-  user_wertc, // 主播使用webrtc直播
-  user_srs, // 主播使用srs直播
-  user_obs, // 主播使用obs/ffmpeg直播
-}
-
-/** 拉流是否需要鉴权 */
-export enum LiveRoomPullIsShouldAuthEnum {
-  /** 需要鉴权 */
-  yes,
-  /** 不需要鉴权 */
-  no,
-}
-
-/** 是否使用cdn */
-export enum LiveRoomUseCDNEnum {
-  /** 使用cdn */
-  yes,
-  /** 不使用cdn */
-  no,
-}
-
-/** 直播间状态 */
-export enum LiveRoomStatusEnum {
-  /** 正常 */
-  normal,
-  /** 禁用 */
-  disable,
-}
-
-/** 直播间是否显示 */
-export enum LiveRoomIsShowEnum {
-  /** 显示 */
-  yes,
-  /** 不显示 */
-  no,
-}
-
-export interface ILiveRoom {
-  id?: number;
-  /** 直播间名称 */
-  name?: string;
-  /** 直播间简介 */
-  desc?: string;
-  /** 直播间备注 */
-  remark?: string;
-  /** 是否使用cdn */
-  cdn?: LiveRoomUseCDNEnum;
-  /** 拉流是否需要鉴权 */
-  pull_is_should_auth?: LiveRoomPullIsShouldAuthEnum;
-  /** 权重 */
-  weight?: number;
-  /** 推流秘钥 */
-  key?: string;
-  /** 直播间类型 */
-  type?: LiveRoomTypeEnum;
-  /** 开播预览图 */
-  cover_img?: string;
-  /** 直播间背景图 */
-  bg_img?: string;
-  /** 直播间状态 */
-  status?: LiveRoomStatusEnum;
-  /** 直播间是否显示 */
-  is_show?: LiveRoomIsShowEnum;
-
-  /** 用户信息 */
-  user?: IUser;
-  /** 用户信息 */
-  users?: IUser[];
-  /** 分区信息 */
-  area?: IArea;
-  /** 分区信息 */
-  areas?: IArea[];
-  /** 直播信息 */
-  live?: ILive;
-  user_live_room?: IUserLiveRoom & { user: IUser };
-
-  rtmp_url?: string;
-  flv_url?: string;
-  hls_url?: string;
-
-  created_at?: string;
-  updated_at?: string;
-  deleted_at?: string;
-}
-export interface IQqUser {
-  id?: number;
-  client_id?: number;
-  openid?: string;
-  unionid?: string;
-  nickname?: string;
-  figureurl?: string;
-  figureurl_1?: string;
-  figureurl_2?: string;
-  figureurl_qq_1?: string;
-  figureurl_qq_2?: string;
-  constellation?: string;
-  gender?: string;
-  city?: string;
-  province?: string;
-  year?: string;
-  created_at?: string;
-  updated_at?: string;
-  deleted_at?: any;
 }
 
 export interface ILink {
