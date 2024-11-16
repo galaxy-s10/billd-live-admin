1ea02a0cb2a109e02cf15b36f927c9da3436d82d
shuisheng <2274751790@qq.com>
2024-11-17 00:44:50 +0800

    fix: 优化

--- src/views/goods/update/index.vue
+++ src/views/goods/update/index.vue
@@ -1,27 +1,18 @@
 <template>
   <div v-if="!route.query.id">请选择商品id</div>
-  <AddCpt
-    v-if="Object.keys(formData).length"
-    :edit="true"
-    v-model="formData"
-  ></AddCpt>
+  <AddCpt :edit="true"></AddCpt>
 </template>
 
 <script lang="ts" setup>
-import { onMounted, ref } from 'vue';
+import { onMounted } from 'vue';
 import { useRoute } from 'vue-router';
-
-import { fetchGoodsFind } from '@/api/googds';
 
 import AddCpt from '../create/index.vue';
 
-const formData = ref({});
 const route = useRoute();
 
-onMounted(async () => {
+onMounted(() => {
   if (!route.query.id) return;
-  const { data } = await fetchGoodsFind(+route.query.id!);
-  formData.value = data;
 });
 </script>
 
